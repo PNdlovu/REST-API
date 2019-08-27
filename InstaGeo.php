41e92bbf46bf6b3cecfd0a82ed848a1e06878f1a
phil <pitso@live.co.uk>
2019-08-27 01:52:46 +0100

    updated googleAPI

--- InstaGeo.php
+++ InstaGeo.php
@@ -29,7 +29,7 @@ if (!empty($_GET['location'])) {
 <head>
     <meta charset="utf-8"/>
     <title>InstaGeo</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="script.js"></script>
 </head>
 <body>
